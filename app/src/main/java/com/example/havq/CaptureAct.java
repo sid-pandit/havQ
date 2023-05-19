acb767f140a1ad7e30fe6203cd8909c1de7c7e29
sid-pandit <siddharth281102@gmail.com>
2023-05-19 09:52:31 +0530

    final commit-1

---
+++ app/src/main/java/com/example/havq/CaptureAct.java
@@ -0,0 +1,7 @@
+package com.example.havq;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity
+{
+}
--- app/src/main/java/com/example/havq/scanner.java
+++ app/src/main/java/com/example/havq/scanner.java
@@ -1,13 +1,11 @@
 package com.example.havq;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -24,22 +22,14 @@ FirebaseFirestore firestore;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
         firestore=FirebaseFirestore.getInstance();
-        Intent saveThe;
-        saveThe=new Intent(scanner.this,MainActivity3.class);
+
 
         qrBtn=findViewById(R.id.qrBtn);
-
-        qrBtn.setOnClickListener(view -> startActivity(saveThe));
         Map<String,Object> user = new HashMap<>();
         user.put("firstName","Tushar");
         user.put("lastName","Malviya");
         user.put("description","hvyCoder");
 
-        Task<DocumentReference> documentReferenceTask = firestore.collection("users").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-            @Override
-            public void onSuccess(DocumentReference documentReference) {
-                Toast.makeText(scanner.this, "Success", Toast.LENGTH_SHORT).show();
-            }
-        }).addOnFailureListener(e -> Toast.makeText(scanner.this, "Failure", Toast.LENGTH_SHORT).show());
+        Task<DocumentReference> documentReferenceTask = firestore.collection("users").add(user).addOnSuccessListener(documentReference -> Toast.makeText(scanner.this, "Success", Toast.LENGTH_SHORT).show()).addOnFailureListener(e -> Toast.makeText(scanner.this, "Failure", Toast.LENGTH_SHORT).show());
     }
 }
